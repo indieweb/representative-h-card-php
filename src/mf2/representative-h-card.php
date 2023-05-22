3831455f814de8e60f100dcac02504010d6c00c8
Marty McGuire <schmartissimo@gmail.com>
2023-05-22 12:48:05 -0400

    build_url: treat empty fragment/query as absent

--- src/mf2/representative-h-card.php
+++ src/mf2/representative-h-card.php
@@ -130,8 +130,8 @@ function build_url($parsed_url) {
   $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : '';
   $pass     = ($user || $pass) ? "$pass@" : '';
   $path     = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-  $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-  $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+  $query    = !empty($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+  $fragment = !empty($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
   return "$scheme$user$pass$host$port$path$query$fragment";
 }
 
